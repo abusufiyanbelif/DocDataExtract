8498af2de47da10e417dbb6068f05ecf6ea5f7b6
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-09-14 21:45:29 +0000

    Try fixing this error: `Console Error: In HTML, <html> cannot be a child

--- src/app/extractor/layout.tsx
+++ src/app/extractor/layout.tsx
@@ -13,17 +13,9 @@ export default function ExtractorLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
-        {children}
-        <Toaster />
-      </body>
-    </html>
+    <>
+      {children}
+      <Toaster />
+    </>
   );
 }
