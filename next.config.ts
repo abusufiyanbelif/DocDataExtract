8dd3111e5165a3a68f06532e573c739b260914b0
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2026-01-28 23:03:48 +0000

    studio-2720090959:~/studio{main}$ npm run build
    > nextn@0.1.0 build
    > n

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   serverExternalPackages: ['handlebars'],
   typescript: {
     ignoreBuildErrors: true,
