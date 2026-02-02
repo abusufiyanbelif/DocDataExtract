a5c46b5c0faeda26703aebc296e9d7cd46ed6d2f
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2026-02-02 03:59:59 +0000

    can you check all my input prompt form the begging and check the last fi

--- next.config.js
+++ next.config.js
@@ -1,22 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'firebasestorage.googleapis.com',
-        },
-        {
-            protocol: 'https',
-            hostname: 'picsum.photos',
-        }
-      ],
-    },
-    allowedDevOrigins: [
-        "http://localhost:3000",
-        "http://localhost:9002"
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
     ],
-  };
-  
-  export default nextConfig;
-  
+  },
+  experimental: {
+    allowedDevOrigins: ['http://localhost:3000', 'http://localhost:9002'],
+  },
+};
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -102,4 +101,4 @@ const config: Config = {
   plugins: [require('tailwindcss-animate')],
 };
 
-export default config;
+module.exports = config;
