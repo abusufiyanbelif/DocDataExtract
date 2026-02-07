cb256d08feae11803aa52929a7bd44b2a6ae6c5d
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2026-02-07 17:34:29 +0000

    Check ttr.txt file and fix tha flaws. Check ./src/app/icon.tsx file and

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -3,11 +3,6 @@ import { ImageResponse } from 'next/server'
 // Route segment config
 export const runtime = 'edge'
 
-// Image metadata
-export const size = {
-  width: 32,
-  height: 32,
-}
 export const contentType = 'image/png'
 
 // Image generation
@@ -31,7 +26,8 @@ export default function Icon() {
       </div>
     ),
     {
-      ...size,
+      width: 32,
+      height: 32,
     }
   )
 }
